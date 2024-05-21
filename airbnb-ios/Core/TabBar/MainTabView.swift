f6acac55d6d31ce7de042f34e545db1b5cbfa2ab
john2796 <jbmiranda22796@gmail.com>
2024-05-20 22:40:33 -0700

    setup folder structure

---
+++ airbnb-ios/Core/TabBar/MainTabView.swift
@@ -0,0 +1,20 @@
+//
+//  MainTabView.swift
+//  airbnb-ios
+//
+//  Created by john benedict miranda on 5/20/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
