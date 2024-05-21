ae35833fa7b6a75eda6092a5d3b3d3239adc306b
john2796 <jbmiranda22796@gmail.com>
2024-05-20 22:24:25 -0700

    initialize project

---
+++ airbnb-ios/airbnb_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  airbnb_iosApp.swift
+//  airbnb-ios
+//
+//  Created by john benedict miranda on 5/20/24.
+//
+
+import SwiftUI
+
+@main
+struct airbnb_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
